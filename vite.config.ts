2d7b6ecc76b9edd75b89220065a8428f482a0b3a
infiniticapital <wandicorcino@gmail.com>
2025-11-10 18:36:21 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',                  // importante para dominio propio en la raíz
+  base: '/',               // dominio propio en la raíz: https://hargna.com
+  build: {
+    outDir: 'docs',        // Vite generará la web final en /docs
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
